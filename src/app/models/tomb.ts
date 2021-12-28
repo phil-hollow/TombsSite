39d3fb789a255515bcf8a0e180900661c95c264a
DESKTOP-79E605K\Phil <oleg.bond9696@gmail.com>
2021-12-29 01:04:45 +0200

    Added products page, logic and data.

---
+++ src/app/models/tomb.ts
@@ -0,0 +1,10 @@
+export class Tomb {
+    id: number = 0
+    name: string | undefined
+    description: string | undefined
+    material: string | undefined
+    price: number | undefined
+    picture: string | undefined
+    constructor() {
+    }
+}
--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { throwIfEmpty } from 'rxjs';
+import { DataService } from 'src/app/data/data.service';
+import { Tomb } from 'src/app/models/tomb';
 
 @Component({
-  selector: 'app-products-page',
-  templateUrl: './products-page.component.html',
-  styleUrls: ['./products-page.component.scss']
+    selector: 'app-products-page',
+    templateUrl: './products-page.component.html',
+    styleUrls: ['./products-page.component.scss']
 })
 export class ProductsPageComponent implements OnInit {
 
-  constructor() { }
+    currentProducts: Array<Tomb> = new Array<Tomb>();
+    products: Tomb[];
+    currentPage: number = 1;
+    pages: number;
+    constructor(private dataService: DataService) {
+        this.products = dataService.GetProducts();
+        this.pages = Math.ceil(this.products.length / 12);
+        this.GetCurrentProducts();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    GetCurrentProducts() {
+
+        let endIndex = this.currentPage * 12;
+        if (endIndex > this.products.length) {
+            this.currentProducts = this.products.slice(this.currentPage * 12 - 12);
+        } else {
+            this.currentProducts = this.products.slice(this.currentPage * 12 - 12, endIndex);
+        }
+    }
+
+    ChangePage(changedNymber: number) {
+        let newPage = changedNymber + this.currentPage;
+        if (newPage <= this.pages && newPage > 0) {
+            this.currentPage = newPage;
+            this.GetCurrentProducts();
+        }
+    }
 }
