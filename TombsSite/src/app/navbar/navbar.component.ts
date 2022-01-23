f9b5a6a1e8b512d7ffc81a394485ea5dc7a6d4fc
DESKTOP-79E605K\Phil <oleg.bond9696@gmail.com>
2022-01-24 01:20:46 +0200

    Maked some corects

--- TombsSite/src/app/navbar/navbar.component.ts
+++ TombsSite/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AdminSessionService } from '../services/admin-session.service';
 import { OrderService } from '../services/order.service';
 
@@ -8,11 +9,16 @@ import { OrderService } from '../services/order.service';
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-    constructor(public orderService:OrderService, public adminSessionService:AdminSessionService) { 
-      
+    constructor(public orderService: OrderService,
+        public adminSessionService: AdminSessionService,
+        private router: Router) {
+
     }
 
     ngOnInit(): void {
     }
-
+    GoToContacts() {
+        this.router.navigate(['/']);
+        window.scroll(66666666, 66666666);
+    }
 }
