282eba78e454a1fe861f9b521eb4bc383d217e5b
DESKTOP-79E605K\Phil <oleg.bond9696@gmail.com>
2022-01-27 01:45:13 +0200

    Ceated admin forms

--- TombsSite/src/app/pages/main-page/main-page.component.ts
+++ TombsSite/src/app/pages/main-page/main-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    //var slides = document.querySelectorAll('.slides .slide');
+    //var currentSlide = 0;
+    //var slideInterval = setInterval(nextSlide, 2000);
+
+    //function nextSlide() {s
+    //  slides[currentSlide].className = 'slide';
+    //  currentSlide = (currentSlide + 1) % slides.length;
+    //  slides[currentSlide].className = 'slide showing';
 }
